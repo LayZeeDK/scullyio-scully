55da36cde8f04c271bf038f145c8d9bf23f622e6
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-01-14 00:39:51 +0100

    test(ng-lib): add idle monitor test suite

--- projects/scullyio/ng-lib/src/lib/idleMonitor/idle-monitor.service.spec.ts
+++ projects/scullyio/ng-lib/src/lib/idleMonitor/idle-monitor.service.spec.ts
@@ -1,21 +1,33 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
-import { IdleMonitorService } from './idle-monitor.service';
+import {IdleMonitorService} from './idle-monitor.service';
 
 describe('IdleMonitorService', () => {
+  function latestDispatchedEvent(): Event | undefined {
+    const [event] = dispatchEventSpy.calls.mostRecent().args;
+
+    return event;
+  }
+
   let service: IdleMonitorService;
+  let dispatchEventSpy: jasmine.Spy;
+
+  beforeEach(() => {
+    spyOn(window, 'dispatchEvent');
+    dispatchEventSpy = window.dispatchEvent as jasmine.Spy;
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule.withRoutes([]),
-      ],
+      imports: [RouterTestingModule.withRoutes([])],
     });
     service = TestBed.inject(IdleMonitorService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('Events', () => {
+    it('dispatches the initialized event immediately', () => {
+      expect(latestDispatchedEvent().type).toBe('AngularInitialized');
+    });
   });
 });
